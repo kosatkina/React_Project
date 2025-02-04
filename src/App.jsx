d349124b1be59ccb71fb85dc85f5863c3acfee4e
kosatkina <tatiana.kosatkina@gmail.com>
2025-02-04 14:45:37 -0500

    Created Header and MainGoal custom components and used them in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function MainGoal() {
+  return (
+    <h3>My main goal: Learn coding with React</h3>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <div> 
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
+      <MainGoal />
     </div>
   );
 }
